b77947f58bf8dd5333cfb259ea1aef96777da8fe
josephkem <josephkemoy@gmail.com>
2020-09-15 20:43:28 -0400

    Modified the mutation query in the users resolever to save users to the database on signup

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,4 +1,7 @@
 const { users, tasks } = require("../constants/index");
+const User = require("../database/models/users");
+const colors = require("colors");
+const bcrypt = require("bcryptjs");
 
 module.exports = {
   Query: {
@@ -6,7 +9,23 @@ module.exports = {
     user: (_, { id }) => users.find((user) => user.id === id),
   },
 
-  Mutation: {},
+  Mutation: {
+    signup: async (_, { input }) => {
+      try {
+        const user = await User.findOne({ email: input.email });
+
+        if (user) {
+          throw new Error("Email already in use");
+        }
+        const hashedPassword = await bcrypt.hash(input.password, 12);
+        const newUser = new User({ ...input, password: hashedPassword });
+        const result = await newUser.save();
+        return result;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
 
   User: {
     tasks: ({ id }) => tasks.filter((task) => task.id === id),
