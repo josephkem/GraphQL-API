8d8d83c74f160c632cd4b8b651c2f782c0192505
josephkem <josephkemoy@gmail.com>
2020-09-22 18:09:18 -0400

    Modified the task typedefs mutation and resolver to update tasks associated with a user

--- resolvers/task.js
+++ resolvers/task.js
@@ -1,5 +1,9 @@
 const uuid = require("uuid");
 const { users, tasks } = require("../constants/index");
+const Task = require("../database/models/tasks");
+const User = require("../database/models/users");
+const { isAuthenticated } = require("./middleware/index");
+const { combineResolvers } = require("graphql-resolvers");
 
 module.exports = {
   Query: {
@@ -11,11 +15,22 @@ module.exports = {
   },
 
   Mutation: {
-    createTask: (_, { input }) => {
-      const task = { ...input, id: uuid.v4() };
-      tasks.push(task);
-      return task;
-    },
+    createTask: combineResolvers(
+      isAuthenticated,
+      async (_, { input }, { email }) => {
+        try {
+          const user = await User.findOne({ email });
+          const task = new Task({ ...input, user: user.id });
+          const result = await task.save();
+          user.tasks.push(result.id);
+          await user.save();
+          return result;
+        } catch (err) {
+          console.log(err);
+          throw err;
+        }
+      }
+    ),
   },
 
   Task: {
