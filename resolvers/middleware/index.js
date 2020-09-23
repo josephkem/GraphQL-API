a4eb64bfb7739c85113188d2bfa2aaf75f35520f
josephkem <josephkemoy@gmail.com>
2020-09-23 01:19:49 -0400

    Modified query to get tasks by id

--- resolvers/middleware/index.js
+++ resolvers/middleware/index.js
@@ -1,5 +1,5 @@
 const { skip } = require("graphql-resolvers");
-const Task = require('../../database/models/tasks')
+const Task = require("../../database/models/tasks");
 
 module.exports.isAuthenticated = (_, __, { email }) => {
   if (!email) {
@@ -9,13 +9,17 @@ module.exports.isAuthenticated = (_, __, { email }) => {
   return skip;
 };
 
-module.exports.isTaskOwner = (_, {id }, { loggedInUserID}) => {
-  const task = await Task.findById(id)
-  if(!task) {
-    throw new Error('Task not found')
-  } else if (task.user !== loggedInUserID) {
-    throw new Error('Not authorized as task owner')
+module.exports.isTaskOwner = async (_, { id }, { loggedInUserId }) => {
+  try {
+    const task = await Task.findById(id);
+    if (!task) {
+      throw new Error("Task not found");
+    } else if (task.user !== loggedInUserId) {
+      throw new Error("Not authorized as task owner");
+    }
+    return skip;
+  } catch (err) {
+    console.log(err);
+    throw err;
   }
-  return skip
-
-}
+};
--- resolvers/task.js
+++ resolvers/task.js
@@ -2,7 +2,7 @@ const uuid = require("uuid");
 const { users, tasks } = require("../constants/index");
 const Task = require("../database/models/tasks");
 const User = require("../database/models/users");
-const { isAuthenticated } = require("./middleware/index");
+const { isAuthenticated, isTaskOwner } = require("./middleware/index");
 const { combineResolvers } = require("graphql-resolvers");
 
 module.exports = {
@@ -19,34 +19,40 @@ module.exports = {
         }
       }
     ),
-    task: combineResolvers(isAuthenticated, (_, { id }) => {
-      return tasks.find((task) => task.id === id);
+    task: combineResolvers(isAuthenticated, isTaskOwner, async (_, { id }) => {
+      try {
+        const task = await Task.findById(id);
+        return task;
+      } catch (err) {
+        console.log(err);
+        throw err;
+      }
     }),
+  },
 
-    Mutation: {
-      createTask: combineResolvers(
-        isAuthenticated,
-        async (_, { input }, { email }) => {
-          try {
-            const user = await User.findOne({ email });
-            const task = new Task({ ...input, user: user.id });
-            const result = await task.save();
-            user.tasks.push(result.id);
-            await user.save();
-            return result;
-          } catch (err) {
-            console.log(err);
-            throw err;
-          }
+  Mutation: {
+    createTask: combineResolvers(
+      isAuthenticated,
+      async (_, { input }, { email }) => {
+        try {
+          const user = await User.findOne({ email });
+          const task = new Task({ ...input, user: user.id });
+          const result = await task.save();
+          user.tasks.push(result.id);
+          await user.save();
+          return result;
+        } catch (err) {
+          console.log(err);
+          throw err;
         }
-      ),
-    },
+      }
+    ),
+  },
 
-    Task: {
-      user: ({ userId }) => {
-        console.log("userId", userId);
-        return users.find((user) => user.id === userId);
-      },
+  Task: {
+    user: ({ userId }) => {
+      console.log("userId", userId);
+      return users.find((user) => user.id === userId);
     },
   },
 };
